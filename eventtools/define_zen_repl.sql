ef12e599517471f4223268d7fcff03fee67de731
Bob Jacobsen <jacobsen@mac.com>
2011-08-19 07:35:13 +0000

    part of Zen Cart schema used by overlap

---
+++ eventtools/define_zen_repl.sql
@@ -0,0 +1,83 @@
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Database: `x2011west`
+--
+
+-- --------------------------------------------------------
+
+-- 
+-- This defines tables needed if you are not using Zen Cart
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gr2012_eventtools_users`
+--
+
+DROP TABLE IF EXISTS `gr2012_customers`;
+
+CREATE TABLE IF NOT EXISTS `gr2012_customers` (
+  `customers_id` int(11) NOT NULL auto_increment,
+  `customers_gender` char(1) NOT NULL default '',
+  `customers_firstname` varchar(32) NOT NULL default '',
+  `customers_lastname` varchar(32) NOT NULL default '',
+  `customers_dob` datetime NOT NULL default '0001-01-01 00:00:00',
+  `customers_email_address` varchar(96) NOT NULL default '',
+  `customers_nick` varchar(96) NOT NULL default '',
+  `customers_default_address_id` int(11) NOT NULL default '0',
+  `customers_telephone` varchar(32) NOT NULL default '',
+  `customers_fax` varchar(32) default NULL,
+  `customers_x2011_nmra_num` varchar(8) default NULL,
+  `customers_x2011_reg_num` varchar(8) default NULL,
+  `customers_x2011_ldsig_num` varchar(32) default NULL,
+  `customers_x2011_opsig_num` varchar(32) default NULL,
+  `customers_x2011_nasg_num` varchar(32) default NULL,
+  `customers_x2011_other_aff_groups` varchar(60) default NULL,
+  `customers_x2011_emerg_contact_name` varchar(60) default NULL,
+  `customers_x2011_emerg_contact_phone` varchar(32) default NULL,
+  `customers_x2011_MMRp` char(1) default NULL,
+  `customers_x2011_HLMp` char(1) default NULL,
+  `customers_x2011_first_name_badge` varchar(32) default NULL,
+  `customers_x2011_spouse_name` varchar(32) default NULL,
+  `customers_x2011_cell_number` varchar(32) default NULL,
+  `customers_x2011_nmra_region` varchar(8) default NULL,
+  `customers_x2011_scale` varchar(32) default NULL,
+  `customers_password` varchar(40) NOT NULL default '',
+  `customers_newsletter` char(1) default NULL,
+  `customers_group_pricing` int(11) NOT NULL default '0',
+  `customers_email_format` varchar(4) NOT NULL default 'TEXT',
+  `customers_authorization` int(1) NOT NULL default '0',
+  `customers_referral` varchar(32) NOT NULL default '',
+  `customers_paypal_payerid` varchar(20) NOT NULL default '',
+  `customers_paypal_ec` tinyint(1) unsigned NOT NULL default '0',
+  `customers_x2011_associated_num` varchar(60) default NULL,
+  PRIMARY KEY  (`customers_id`),
+  KEY `idx_email_address_zen` (`customers_email_address`),
+  KEY `idx_referral_zen` (`customers_referral`(10)),
+  KEY `idx_grp_pricing_zen` (`customers_group_pricing`),
+  KEY `idx_nick_zen` (`customers_nick`),
+  KEY `idx_newsletter_zen` (`customers_newsletter`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1428 ;
+
+DROP TABLE IF EXISTS `gr2012_address_book`;
+
+CREATE TABLE IF NOT EXISTS `gr2012_address_book` (
+  `address_book_id` int(11) NOT NULL auto_increment,
+  `customers_id` int(11) NOT NULL default '0',
+  `entry_gender` char(1) NOT NULL default '',
+  `entry_company` varchar(64) default NULL,
+  `entry_firstname` varchar(32) NOT NULL default '',
+  `entry_lastname` varchar(32) NOT NULL default '',
+  `entry_street_address` varchar(64) NOT NULL default '',
+  `entry_suburb` varchar(32) default NULL,
+  `entry_postcode` varchar(10) NOT NULL default '',
+  `entry_city` varchar(32) NOT NULL default '',
+  `entry_state` varchar(32) default NULL,
+  `entry_country_id` int(11) NOT NULL default '0',
+  `entry_zone_id` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`address_book_id`),
+  KEY `idx_address_book_customers_id_zen` (`customers_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=816 ;
