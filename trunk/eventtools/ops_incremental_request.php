014e3b3ee72d5d7f95695aeb1c76e1a287c244c1
jacobsen <jacobsen@f600a6c0-e53c-4c81-ba93-d4b609af4611>
2012-01-15 05:49:47 +0000

    new type

--- trunk/eventtools/ops_incremental_request.php
+++ trunk/eventtools/ops_incremental_request.php
@@ -15,8 +15,8 @@ mysql_connect($opts['hn'],$opts['un'],$opts['pw']);
 $email = $_REQUEST[ "email" ];
 
 foreach ( $values as $k ) {
-    echo " increment(".$k.") ";
     $value = $_REQUEST[ $k ];
+    echo " increment(".$k.",".$email.",".$value.") ";
     mysql_query("INSERT INTO ".$event_tools_db_prefix."eventtools_user_request_entry (email, key, value) VALUES ('".$_REQUEST[ "email" ]."','".$k."','".$value."');");    
 }
 ?>
