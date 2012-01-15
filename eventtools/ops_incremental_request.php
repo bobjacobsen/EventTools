b72d757f713abaed85f0797a5888384180f2ea01
Bob Jacobsen <jacobsen@mac.com>
2012-01-15 06:59:39 +0000

    new type

--- eventtools/ops_incremental_request.php
+++ eventtools/ops_incremental_request.php
@@ -15,7 +15,7 @@ $email = $_REQUEST[ "email" ];
 foreach ( $values as $k ) {
     $value = $_REQUEST[ $k ];
     echo " insert(".$k.",".$email.",".$value.") ";
-    echo "INSERT INTO ".$event_tools_db_prefix."eventtools_user_request_entry (email, key, value) VALUES ('".$email."','".$k."','".$value."');";
+    echo "INSERT INTO ".$event_tools_db_prefix."eventtools_user_request_entry (`email`, `key`, `value`) VALUES ('".$email."','".$k."','".$value."');";
     echo " r=".mysql_insert_id()." "; 
 }
 ?>
