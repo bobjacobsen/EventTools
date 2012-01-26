38838043dd6ef4a3a2f75934c36ba0771c89268b
jacobsen <jacobsen@f600a6c0-e53c-4c81-ba93-d4b609af4611>
2012-01-26 06:23:51 +0000

    add cross-load

--- trunk/eventtools/ops_incremental_request.php
+++ trunk/eventtools/ops_incremental_request.php
@@ -3,6 +3,19 @@
 // the contents of the $_REQUEST array called out by
 // the values array.
 
+function check_for_value(values, check, reqs)
+{
+    // look through values array, checking the layout items for a match with a specific value
+    foreach ( $values as $k ) {
+        if ((substr($k, 0, 2) === "v_") { // is a layout
+            $value = $_REQUEST[ $k ];
+            if ($value == $check) { // got it, add layout to requests
+                $reqs[] = $value
+            }
+        }
+    }
+}
+
 global $values;
 
 global $opts, $event_tools_db_prefix;
@@ -18,5 +31,20 @@ foreach ( $values as $k ) {
     mysql_query($op);
     // echo " r=".mysql_insert_id()." <br/>"; 
 }
+
+// now we try to save a reqular format request
+$reqs = array();
+
+// run the requests
+check_for_value(values, "on", reqs);
+check_for_value(values, "1", reqs);
+check_for_value(values, "2", reqs);
+check_for_value(values, "3", reqs);
+check_for_value(values, "4", reqs);
+check_for_value(values, "5", reqs);
+check_for_value(values, "6", reqs);
+
+print reqs;
+
 ?>
 
