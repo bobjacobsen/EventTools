0801b2535156d7e21cc1ac6ad60629fc87a329dd
jacobsen <jacobsen@f600a6c0-e53c-4c81-ba93-d4b609af4611>
2012-01-30 07:46:24 +0000

    up to 9 priorities

--- trunk/eventtools/ops_incremental_request.php
+++ trunk/eventtools/ops_incremental_request.php
@@ -38,7 +38,7 @@ foreach ( $values as $k ) {
 // now we try to save a reqular format request
 $reqs = array();
 
-// run the requests
+// run the requests - need one for each priority that is given on page
 $reqs = check_for_value($values, "on", $reqs);
 $reqs = check_for_value($values, "1", $reqs);
 $reqs = check_for_value($values, "2", $reqs);
@@ -46,6 +46,9 @@ $reqs = check_for_value($values, "3", $reqs);
 $reqs = check_for_value($values, "4", $reqs);
 $reqs = check_for_value($values, "5", $reqs);
 $reqs = check_for_value($values, "6", $reqs);
+$reqs = check_for_value($values, "7", $reqs);
+$reqs = check_for_value($values, "8", $reqs);
+$reqs = check_for_value($values, "9", $reqs);
 
 // so reqs now is an ordered list of requests
 
