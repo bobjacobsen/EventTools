fac7dc2cf821332f79658ecfb0ee800cea862b73
Bob Jacobsen <jacobsen@mac.com>
2012-04-08 03:24:21 +0000

    oiptions

--- eventtools/ops_assign_email_operator.php
+++ eventtools/ops_assign_email_operator.php
@@ -5,10 +5,50 @@
 require_once('access.php');
 require_once('utilities.php');
 
+// for cycle defined
+if (! ($args["cy"]) ) {
+    echo "This is the page for emailing to the operators.<p/>";
+    echo "Please fill in the form and press 'start'. All fields are required. Multiple email addresses can be specified, separated with a comma. Put a dollar sign '$' in 'test' to send for real.";
+    echo '<form method="get" action="ops_assign_email_operator.php">
+        Cycle Name: <input  name="cy"><br>
+        From email address: <input  name="from"><br>
+        Bcc email address(es): <input  name="bcc"><br>
+        (Test) To email address(es): <input  name="testto"><br>
+        <button type="submit">Start</button>
+        </form>
+    ';
+    
+    // display existing cycles & number of assignments)
+    echo '<h3>Existing cycles</h3><table><tr><th>Cycle Name</th><th>N Assigned</th></tr>';
+    global $opts, $event_tools_db_prefix, $event_tools_href_add_on;
+    mysql_connect($opts['hn'],$opts['un'],$opts['pw']);
+    @mysql_select_db($opts['db']) or die( "Unable to select database");
+    
+    $query="
+        SELECT opsreq_group_cycle_name, SUM(status) 
+        FROM ".$event_tools_db_prefix."eventtools_ops_group_session_assignments
+        WHERE status = 1
+        GROUP BY opsreq_group_cycle_name
+        ORDER BY  opsreq_group_cycle_name
+        ;
+    ";
+    $result=mysql_query($query);
+    $num = mysql_numrows($result);
+    for ($i = 0; $i < $num; $i++) {
+        echo '<tr><td>'.mysql_result($result,$i,"opsreq_group_cycle_name").'</td><td>'.mysql_result($result,$i,1).'</td></tr>';
+    }
+    echo '</table>';
+    return;
+} else {
+    $cycle = $args["cy"];
+    $from = $args["from"];
+    $bcc = $args["bcc"];
+    $testto = $args["testto"];
+}
+
 mysql_connect($opts['hn'],$opts['un'],$opts['pw']);
 @mysql_select_db($opts['db']) or die( "Unable to select database");
 
-$cycle = "110511_3jpz";
 echo "Emailing from cycle ".$cycle."<p>";
 
 $query="
@@ -47,15 +87,16 @@ while ($i < $num) {
             $lastmajorkey = mysql_result($result,$i+1,"opsreq_person_email");
         }
         
-        echo "sending to ".mysql_result($result,$i,"opsreq_person_email").'<p>';
-    
-        $to = mysql_result($result,$i,"opsreq_person_email");
-        //$to = "rgj1927@pacbell.net";
-        
+        if ($testto=='$')
+            $to = mysql_result($result,$i,"opsreq_person_email");
+        else
+            $to = $testto;
+
+        echo "sending to ".mysql_result($result,$i,"opsreq_person_email").'('.$to.')<p>';
+            
         $subject = $event_tools_event_name." Operating Session Assignments";
         
-        $headers = "from: rrjim@aol.com\nbcc: rrjim@aol.com, rgj1927@pacbell.net";
-        //$headers = "from: jacobsen@berkeley.edu";
+        $headers = "from: ".$from."\nbcc: ".$bcc;
                 
         $body = $part1.$sessions."\n\n\n";
         
