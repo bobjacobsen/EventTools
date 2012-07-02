450ddc8ef2b93ed7791b5f43f49017501b3c3425
jacobsen <jacobsen@f600a6c0-e53c-4c81-ba93-d4b609af4611>
2012-07-02 19:24:50 +0000

    basic statement from x2011

--- trunk/zencart-code/includes/languages/english/html_includes/nmra2013/define_privacy.php
+++ trunk/zencart-code/includes/languages/english/html_includes/nmra2013/define_privacy.php
@@ -1,3 +1,18 @@
-<p><strong>Privacy Sample Text ...</strong></p>
-<p>This section of text is from the Define Pages Editor located under Tools in the Admin.</p>
-<p>To remove this section of the text, delete it from the Define Pages Editor.</p>
+<p>
+<b>Our Commitment to Privacy</b><br>
+Your privacy is important to us, and we are committed to maintaining your confidence and trust. For that reason, we have established this Privacy Policy. Any personal information provided by customers/visitors to this website is used for internal purposes only. We will never sell or lease any personal information, customer listings, or aggregate data gathered through this site to any outside party.
+<p>
+<b>Information Collected and How We Use It</b><br>
+We collect two types of information about visitors:
+<p>
+Personal information, which you have voluntarily submitted to us through our information request, on-line ordering or e-mail address.
+<p>
+Aggregate information, automatically collected non-identifying statistics, (such as information about how many visitors log on to our sites, the pages accessed, etc.) Our web servers collect the IP addresses of visitors, not the e-mail addresses. We use this information to measure the use of our site, to improve the content of our site, and to monitor access for security purposes.
+<p>
+<b>Links to Other Sites</b><br>
+This site provides links to a number of third-party external sites. The inclusion of any link does not imply endorsement of the external site company, its website, or the services it promotes or sells. When you leave this site to visit one of these linked sites, the only information transferred to the third party is the fact that you came from www.X2011West.org (the "refer address").
+<p>
+We are not responsible or liable for the independent privacy policies of these third party sites. You should consult the privacy policies at these sites to determine how your information may be used.
+<p>
+<b>Contact Us</b><br>
+Should you have other questions or concerns about this Privacy Policy, please contact us.
