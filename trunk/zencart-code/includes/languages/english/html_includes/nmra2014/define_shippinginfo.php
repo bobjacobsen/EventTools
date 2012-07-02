e153b2d5388976e7a1f6d38f5bf846625f8c363e
jacobsen <jacobsen@f600a6c0-e53c-4c81-ba93-d4b609af4611>
2012-07-02 22:32:35 +0000

    start 2014 customization from current 2013

---
+++ trunk/zencart-code/includes/languages/english/html_includes/nmra2014/define_shippinginfo.php
@@ -0,0 +1,68 @@
+<b>General Refund Policy</b>
+<P>
+Upon cancellation by an attendee, the appropriate refund will be issued in like payment. Please see specific refund criteria below. Allow up to 6 weeks processing time. Refunds applied for after June 15, 2011 will not be processed until 6 weeks after the end of the convention. Please note that payment made by credit card will be refunded to your credit card. 
+<P>
+Separate policies have been established for registration fees, tours, events, merchandise, etc. Please read each section carefully for cancellation/refund criteria. 
+<P>
+<b>Cancellation of Extra Fare Event, Store Merchandise, Extra Fare Clinic or Tour </b>
+<P>
+In the case of a cancellation by the convention, if an extra fare event, store, merchandise, extra fare clinic or tour is canceled prior to the convention, a notice will be sent to the attendee informing of the cancellation. A refund will be processed after 30 days of the notice if no other selection is made by the attendee. 
+<P>
+Last minute cancellations by the convention may be exchanged for other available events, tours, etc. Refunds will not be processed until after the convention. Please allow up to 6 weeks processing time.
+<P>
+<b>Registration Refunds</b>
+<P>
+Cancellation requests should be received in writing either by standard mail (X2011 West Registrar, 530 Fig Tree Lane, Martinez, CA 94553) or by e-mail (<a href="mailto:x2011registrar@aol.com">x2011registrar@aol.com</a>). The following registration refund schedule applies to each individual registrant:
+<ul>
+<li>Prior to May 1, 2011 - a refund of the registration fee less a $25.00 administrative fee per individual registrant will apply.
+<li>May 1, 2011 to June 15, 2011 - A refund of the registration fee less 25%, per individual registrant, will apply (minimum $25.00).
+<li>After June 15, 2011 and prior to start of convention - A refund of the registration fee less 50%, per individual registrant, will apply (minimum $25.00).
+<li>No refund of the registration fee will be provided after the opening of the convention except upon approval of the X2011West Registrar. 
+</ul>
+<b>Tour Refunds for Cancelled Registrations</b>
+<ul>
+<li>Prior to June 15, 2011 - 100% refund of paid tours.
+<li>After June 15, 2011, and prior to the opening of the convention, tour fees will be fully refunded only if the tour has sold out, the tickets have been resold, or if the tour was canceled by the convention. 
+<li>After June 15, 2011 - all tickets eligible for resale must be presented to the Tour Desk (at the convention) on a consignment basis only. The Convention does not guarantee that consigned tickets will be resold.
+</ul>
+<b>Event Refunds for Cancelled Registrations</b>
+<ul>
+<li>Prior to June 15, 2011 - 100% refund of paid events.
+<li>After June 15, 2011 and prior to the opening of the convention, non-food event fees will be fully refunded only if the non-food event has sold out, the tickets have been resold or if the non-food event was canceled by the convention. 
+<li>After June 15, 2011 - all tickets eligible for resale must be presented to the Registration Desk (at the convention) on a consignment basis only. The Convention does not guarantee that consigned tickets will be resold. 
+</ul>
+<b>Extra Fare Food Events Refunds for Cancelled Registrations</b>
+<P>
+Banquet tickets or food event tickets may be returned for refund until 3 business days prior to the Banquet or event. No refunds for the banquet or event will be issued after the final food commitment is made by the convention manager (whichever date comes first).
+<P>
+During the convention, food event tickets may be presented to the Registration Desk for reselling on a consignment basis only. 
+<P>
+The Convention does not guarantee that consigned tickets will be resold.
+<P>
+<b>Merchandise Refunds for Cancelled Registrations</b>
+<ul>
+<li>100% refund of paid merchandise if received by May 11, 2011 (This is the advance order cut-off date) 
+</ul>
+If the request is received after the cut-off date, the merchandise will be shipped to the registrant and appropriate shipping charges will be added to your account. 
+<P>
+No refunds for merchandise cancellations after the cut-off date will be issued. 
+<P>
+<b>Extra Fare Clinic Refunds for Cancelled Registrations</b>
+<ul>
+<li>100% refund of paid clinics if received prior to June 15, 2011 
+</ul>
+Cancellations after June 15, 2011 are eligible for a refund only if the clinic space is subsequently resold. During the convention, Extra Fare Clinic tickets may be presented to the Registration Desk for resale on a consignment basis only.
+<P>
+The Convention does not guarantee that consigned tickets will be resold.
+<P>
+<b>Auction Refunds</b>
+<P>
+The convention offers no guarantee on the condition of the merchandise offered at the Auction and the buyer is purchasing the merchandise at their own risk. No refunds are provided under any circumstance for the Auction. 
+<P>
+<b>Tour & Event Tickets </b>
+<P>
+Tour and event tickets may be presented during the convention for reselling on a consignment basis only. Tour tickets must be turned in to the Tour Desk. Event tickets must be turned in to the convention Registration Desk.
+<P>
+The respective areas will attempt to sell the consigned tickets to other attendees. If successful, the attendee will receive a refund for the ticket value by 6 weeks after of the close of the convention. If the tickets are not resold, the attendee will not receive a refund for the consigned tickets.
+<P>
+The Convention does not guarantee that consigned tickets will be resold. 
