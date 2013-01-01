fbb031fb0a37a1c71d6422d61fe8bb2cdf43a572
Bob Jacobsen <jacobsen@mac.com>
2013-01-01 10:07:17 +0000

    better address record handling, not properly using the default link yet

--- eventtools/ops_incremental_add_prereg.php
+++ eventtools/ops_incremental_add_prereg.php
@@ -33,7 +33,7 @@ $reqs = mysql_query($findu);
 
 if (mysql_num_rows($reqs) == 0) {
  
-    // do an insert of the user
+    // no, do an insert of the user
     $user = "REPLACE INTO ".$event_tools_db_prefix."customers (`customers_email_address`, `customers_firstname`, `customers_lastname`, `customers_telephone`, `customers_cellphone`, `customers_create_date`) VALUES "
         ."('".$email."','".$_REQUEST["fname"]."','".$_REQUEST["lname"]."','".$_REQUEST["phone"]."','".$_REQUEST["cell"]."',now());";
     //print '[ '.$user.' ] ';
@@ -41,7 +41,7 @@ if (mysql_num_rows($reqs) == 0) {
 
 } else {
 
-    // do an update of the user
+    // yes, do an update of the user
     $user = "UPDATE ".$event_tools_db_prefix."customers SET customers_firstname ='".$_REQUEST["fname"]."', customers_lastname ='".$_REQUEST["lname"]."', customers_telephone ='".$_REQUEST["phone"]."', customers_cellphone ='".$_REQUEST["cell"]."' WHERE customers_email_address = '".$email."'";
     //print '[ '.$user.' ] ';
     mysql_query($user);
@@ -49,13 +49,15 @@ if (mysql_num_rows($reqs) == 0) {
 }
 
 // do an insert of the address block
-$findu = "SELECT customers_id FROM ".$event_tools_db_prefix."customers WHERE customers_email_address = '".$email."';";
+// following should instead properly handle prefix_customers.customers_default_address_id instead of just forcing one
+// Right now, it just updates the earliest if there already is one
+$findu = "SELECT ".$event_tools_db_prefix."customers.customers_id, address_book_id FROM (".$event_tools_db_prefix."customers LEFT JOIN ".$event_tools_db_prefix."address_book ON ".$event_tools_db_prefix."customers.customers_id = ".$event_tools_db_prefix."address_book.customers_id ) WHERE customers_email_address = '".$email."';";
 //print '[ '.$findu.' ] ';
 $reqs = mysql_query($findu);
-//print "<p>found ".mysql_result($reqs,$i,"customers_id")."</p>";
+//print "<p>found ".mysql_result($reqs,0,"customers_id")."</p>";
 
-$address = "REPLACE INTO ".$event_tools_db_prefix."address_book (`customers_id`, `entry_city`, `entry_state`) VALUES "
-    ."('".mysql_result($reqs,$i,"customers_id")."','".$_REQUEST["city"]."','".$_REQUEST["state"]."');";
+$address = "REPLACE INTO ".$event_tools_db_prefix."address_book (`customers_id`, `address_book_id`, `entry_street_address`, `entry_city`, `entry_state`, `entry_postcode`) VALUES "
+    ."('".mysql_result($reqs,0,"customers_id")."','".mysql_result($reqs,0,"address_book_id")."','".$_REQUEST["street"]."','".$_REQUEST["city"]."','".$_REQUEST["state"]."','".$_REQUEST["zip"]."');";
 //print '[ '.$address.' ] ';
 mysql_query($address);
 
--- eventtools/ops_incremental_request.php
+++ eventtools/ops_incremental_request.php
@@ -65,17 +65,39 @@ $op = "REPLACE INTO ".$event_tools_db_prefix."eventtools_opsession_req (`opsreq_
 //print 'Request [ '.$op.' ] <p>';
 mysql_query($op);
 
-// do an insert of the user
-$user = "REPLACE INTO ".$event_tools_db_prefix."customers (`customers_email_address`, `customers_firstname`, `customers_lastname`, `customers_telephone`, `customers_cellphone`) VALUES "
-    ."('".$email."','".$_REQUEST["fname"]."','".$_REQUEST["lname"]."','".$_REQUEST["phone"]."','".$_REQUEST["cell"]."');";
-//print 'User [ '.$user.' ] <p>';
-mysql_query($user);
+// see if the user already exists
+$findu = "SELECT customers_id FROM ".$event_tools_db_prefix."customers WHERE customers_email_address = '".$email."';";
+$reqs = mysql_query($findu);
 
-$id = mysql_insert_id();
-$addr = "REPLACE INTO ".$event_tools_db_prefix."address_book (`customers_id`, `entry_street_address`, `entry_city`, `entry_state`, `entry_postcode`) VALUES "
-    ."('".$id."','".$_REQUEST["street"]."','".$_REQUEST["city"]."','".$_REQUEST["state"]."','".$_REQUEST["zip"]."');";
-//print 'Addr [ '.$addr.' ] <p>';
-mysql_query($addr);
+if (mysql_num_rows($reqs) == 0) {
+ 
+    // no, do an insert of the user
+    $user = "REPLACE INTO ".$event_tools_db_prefix."customers (`customers_email_address`, `customers_firstname`, `customers_lastname`, `customers_telephone`, `customers_cellphone`, `customers_create_date`) VALUES "
+        ."('".$email."','".$_REQUEST["fname"]."','".$_REQUEST["lname"]."','".$_REQUEST["phone"]."','".$_REQUEST["cell"]."',now());";
+    //print '[ '.$user.' ] ';
+    mysql_query($user);
+
+} else {
+
+    // yes, do an update of the user
+    $user = "UPDATE ".$event_tools_db_prefix."customers SET customers_firstname ='".$_REQUEST["fname"]."', customers_lastname ='".$_REQUEST["lname"]."', customers_telephone ='".$_REQUEST["phone"]."', customers_cellphone ='".$_REQUEST["cell"]."' WHERE customers_email_address = '".$email."'";
+    //print '[ '.$user.' ] ';
+    mysql_query($user);
+
+}
+
+// do an insert of the address block
+// following should instead properly handle prefix_customers.customers_default_address_id instead of just forcing one
+// Right now, it just updates the earliest if there already is one
+$findu = "SELECT ".$event_tools_db_prefix."customers.customers_id, address_book_id FROM (".$event_tools_db_prefix."customers LEFT JOIN ".$event_tools_db_prefix."address_book ON ".$event_tools_db_prefix."customers.customers_id = ".$event_tools_db_prefix."address_book.customers_id ) WHERE customers_email_address = '".$email."';";
+//print '[ '.$findu.' ] ';
+$reqs = mysql_query($findu);
+//print "<p>found ".mysql_result($reqs,0,"customers_id")."</p>";
+
+$address = "REPLACE INTO ".$event_tools_db_prefix."address_book (`customers_id`, `address_book_id`, `entry_street_address`, `entry_city`, `entry_state`, `entry_postcode`) VALUES "
+    ."('".mysql_result($reqs,0,"customers_id")."','".mysql_result($reqs,0,"address_book_id")."','".$_REQUEST["street"]."','".$_REQUEST["city"]."','".$_REQUEST["state"]."','".$_REQUEST["zip"]."');";
+//print '[ '.$address.' ] ';
+mysql_query($address);
 
 ?>
 
