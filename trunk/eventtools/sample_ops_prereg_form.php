0c64eacab8e095c562aa4a9752d9356773285037
jacobsen <jacobsen@f600a6c0-e53c-4c81-ba93-d4b609af4611>
2013-01-01 10:06:24 +0000

    sample prereq form

---
+++ trunk/eventtools/sample_ops_prereg_form.php
@@ -0,0 +1,137 @@
+<?
+	require_once('access.php'); 
+	require('utilities.php'); 
+
+        if ($_REQUEST['radio1'] != 'none') $_REQUEST['v_'.$_REQUEST['radio1']] = '11';
+        if ($_REQUEST['radio3'] != 'none') $_REQUEST['v_'.$_REQUEST['radio3']] = '12';
+
+	$message = "<span style=\"color:red;font-weight:bold\">";
+
+	if ( !formcomplete() ) {
+		if ( $_SERVER['REQUEST_METHOD'] == 'POST' )
+			$message .= "Please complete all values";
+		$message .= "</span>";
+		showform( $message );
+		return;
+	}
+
+	$now = time();	
+
+	// add to local log file.
+	$datastring = get_request_args(date("m/d/y g:i a T",$now),";")."\r\n";
+	if (!$fp = fopen("datafile.dtf", "a")) {
+	   print "Cannot open \"datafile.dtf\"\n";
+	} else {
+	   fwrite( $fp, $datastring );
+	   fclose( $fp );
+    }
+	// send email with info
+
+    // If needed, change the email address on the next line
+	$to = $event_tools_notify_email_address;
+	
+	$subject = $event_tools_event_name." Registration FYI ".$_REQUEST["fname"]." ".$_REQUEST["lname"];
+	$headers = sprintf("From:  ".$event_tools_event_name." Registration Form <Registrar@BayRails.com>\r\n");
+	$message = sprintf("%s %s has registered\r\n\r\n", $_REQUEST["fname"], $_REQUEST["lname"]);
+	$message .= "A tabular version can be found at http://www.bayrails.com/eventtools/edit_ops_all.php\r\n\r\n";
+
+    // add fill dump
+	$message .= get_request_args(date("m/d/y g:i a T",$now)."\r\n","\r\n");
+
+    // and send
+	mail($to, $subject, $message, $headers);
+	
+	// process into database
+	require( 'ops_incremental_add_prereg.php');
+
+	// show confirmation on page
+	$page = <<<END
+	<h1>Thank you for Registering for BayRails V</h1>
+	Your info has been emailed to $to, stored and will be processed shortly.<br><br>
+	You can expect to hear from us within the next few days, and if you think you've been forgotten or
+	are being ignored, please feel free to send a direct email plea to $to .<br><br>
+	
+END;
+	print $page;
+
+	// end of code here
+
+// check that required values are present
+function formcomplete() {
+
+	$musthave = array();
+	$musthave[] = "fname";
+	$musthave[] = "lname";
+	$musthave[] = "email";
+	$musthave[] = "phone";
+	$musthave[] = "street";
+	$musthave[] = "city";
+	$musthave[] = "state";
+	$musthave[] = "zip";
+	
+	foreach ( $musthave as $i ) {
+		if ( !isset( $_REQUEST[ $i ] ) || ($_REQUEST[ $i ] == "") )
+			return(0);
+	}
+	return(1);
+}
+
+// show the HTML form
+function showform( $message ) {
+    global $event_tools_event_name;
+    
+	 print $message;
+	 $page =  <<<END
+
+    <html>
+    <body>
+	<h2>Register for 
+END;
+	print $page.$event_tools_event_name;
+	 $page =  <<<END
+</h2>
+
+	<form action="" method="get">
+	<table border=1 class="VisTable">
+	<tr valign="top"><td width="40%">
+		<table border=0>
+			<tr><td colspan=2>Who are you?</td></tr>
+			<tr><td>First Name</td><td><input type=text size=25 name=fname></td></tr>
+			<tr><td>Last Name</td><td><input type=text size=25 name=lname></td></tr>
+			<tr><td>Email Address</td><td><input type=text size=25 name=email></td></tr>
+			<tr><td>Telephone</td><td><input type=text size=15 name=phone></td></tr>
+			<tr><td>Cell Phone</td><td><input type=text size=15 name=cell></td></tr>
+			<tr><td>Street Address</td><td><input type=text size=25 name=street></td></tr>
+			<tr><td>City</td><td><input type=text size=25 name=city></td></tr>
+			<tr><td>State</td><td><input type=text size=15 name=state></td></tr>
+			<tr><td>ZIP code</td><td><input type=text size=15 name=zip></td></tr>
+END;
+
+	print $page;
+
+	require('ops_req_tools.php');
+	create_option_entries();
+
+$page =  <<<END
+
+		</table>
+		<hr/>
+	</td><td width="40%">
+		
+	</td>
+	</tr>
+	<tr><td colspan=3><br>Comments<div style="text-align:center"><textarea name="comments" rows=10 cols=60></textarea></div><br></td></tr>
+	</table>
+
+	<input type=submit value="Submit registration">&nbsp;&nbsp;
+	<input type=reset value="Clear this form">
+	</form>
+    </body>
+    </html>
+    
+END;
+	print $page;
+}
+
+
+?>
