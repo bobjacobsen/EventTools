55e0376351ad95fcae49fea12ff5edd1ebb38299
jacobsen <jacobsen@f600a6c0-e53c-4c81-ba93-d4b609af4611>
2013-01-04 08:42:53 +0000

    clean data before DB store

--- trunk/eventtools/ops_incremental_request.php
+++ trunk/eventtools/ops_incremental_request.php
@@ -32,13 +32,18 @@ function check_for_value($values, $check, $reqs)
     return $reqs;
 }
 
+function clean_text($text) 
+{
+    return mysql_real_escape_string(stripslashes($text));
+}
+
 global $values;
 
 global $opts, $event_tools_db_prefix;
 mysql_connect($opts['hn'],$opts['un'],$opts['pw']);
 @mysql_select_db($opts['db']) or die( "Unable to select database");
 
-$email = $_REQUEST[ "email" ];
+$email = clean_text($_REQUEST[ "email" ]);
 
 // save op session request
 $reqs = array();
@@ -61,7 +66,7 @@ $reqs = check_for_value($values, "12", $reqs);
 
 // do an insert of the request
 $op = "REPLACE INTO ".$event_tools_db_prefix."eventtools_opsession_req (`opsreq_person_email`, `opsreq_pri1`, `opsreq_pri2`, `opsreq_pri3`, `opsreq_pri4`, `opsreq_pri5`, `opsreq_pri6`, `opsreq_pri7`, `opsreq_pri8`, `opsreq_pri9`, `opsreq_pri10`, `opsreq_pri11`, `opsreq_pri12`, `opsreq_opt1`, `opsreq_opt2`, `opsreq_opt3`, `opsreq_opt4`, `opsreq_opt5`, `opsreq_opt6`, `opsreq_opt7`, `opsreq_opt8`, `opsreq_comment`) VALUES "
-    ."('".$email."','".$reqs[0]."','".$reqs[1]."','".$reqs[2]."','".$reqs[3]."','".$reqs[4]."','".$reqs[5]."','".$reqs[6]."','".$reqs[7]."','".$reqs[8]."','".$reqs[9]."','".$reqs[10]."','".$reqs[11]."','".$_REQUEST['opt1']."','".$_REQUEST['opt2']."','".$_REQUEST['opt3']."','".$_REQUEST['opt4']."','".$_REQUEST['opt5']."','".$_REQUEST['opt6']."','".$_REQUEST['opt7']."','".$_REQUEST['opt8']."','".$_REQUEST[ "comments" ]."');";
+    ."('".$email."','".$reqs[0]."','".$reqs[1]."','".$reqs[2]."','".$reqs[3]."','".$reqs[4]."','".$reqs[5]."','".$reqs[6]."','".$reqs[7]."','".$reqs[8]."','".$reqs[9]."','".$reqs[10]."','".$reqs[11]."','".clean_text($_REQUEST['opt1'])."','".clean_text($_REQUEST['opt2'])."','".clean_text($_REQUEST['opt3'])."','".clean_text($_REQUEST['opt4'])."','".clean_text($_REQUEST['opt5'])."','".clean_text($_REQUEST['opt6'])."','".clean_text($_REQUEST['opt7'])."','".clean_text($_REQUEST['opt8'])."','".clean_text($_REQUEST[ "comments" ])."');";
 //print 'Request [ '.$op.' ] <p>';
 mysql_query($op);
 
@@ -73,14 +78,14 @@ if (mysql_num_rows($reqs) == 0) {
  
     // no, do an insert of the user
     $user = "REPLACE INTO ".$event_tools_db_prefix."customers (`customers_email_address`, `customers_firstname`, `customers_lastname`, `customers_telephone`, `customers_cellphone`, `customers_create_date`) VALUES "
-        ."('".$email."','".$_REQUEST["fname"]."','".$_REQUEST["lname"]."','".$_REQUEST["phone"]."','".$_REQUEST["cell"]."',now());";
+        ."('".$email."','".clean_text($_REQUEST["fname"])."','".clean_text($_REQUEST["lname"])."','".clean_text($_REQUEST["phone"])."','".clean_text($_REQUEST["cell"])."',now());";
     //print '[ '.$user.' ] ';
     mysql_query($user);
 
 } else {
 
     // yes, do an update of the user
-    $user = "UPDATE ".$event_tools_db_prefix."customers SET customers_firstname ='".$_REQUEST["fname"]."', customers_lastname ='".$_REQUEST["lname"]."', customers_telephone ='".$_REQUEST["phone"]."', customers_cellphone ='".$_REQUEST["cell"]."' WHERE customers_email_address = '".$email."'";
+    $user = "UPDATE ".$event_tools_db_prefix."customers SET customers_firstname ='".clean_text($_REQUEST["fname"])."', customers_lastname ='".clean_text($_REQUEST["lname"])."', customers_telephone ='".clean_text($_REQUEST["phone"])."', customers_cellphone ='".clean_text($_REQUEST["cell"])."' WHERE customers_email_address = '".clean_text($email)."'";
     //print '[ '.$user.' ] ';
     mysql_query($user);
 
@@ -95,7 +100,7 @@ $reqs = mysql_query($findu);
 //print "<p>found ".mysql_result($reqs,0,"customers_id")."</p>";
 
 $address = "REPLACE INTO ".$event_tools_db_prefix."address_book (`customers_id`, `address_book_id`, `entry_street_address`, `entry_city`, `entry_state`, `entry_postcode`) VALUES "
-    ."('".mysql_result($reqs,0,"customers_id")."','".mysql_result($reqs,0,"address_book_id")."','".$_REQUEST["street"]."','".$_REQUEST["city"]."','".$_REQUEST["state"]."','".$_REQUEST["zip"]."');";
+    ."('".mysql_result($reqs,0,"customers_id")."','".mysql_result($reqs,0,"address_book_id")."','".clean_text($_REQUEST["street"])."','".clean_text($_REQUEST["city"])."','".clean_text($_REQUEST["state"])."','".clean_text($_REQUEST["zip"])."');";
 //print '[ '.$address.' ] ';
 mysql_query($address);
 
