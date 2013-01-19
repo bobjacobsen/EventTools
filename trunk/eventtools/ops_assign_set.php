b55c53858829d49b947efa610fed6b2881927d3a
jacobsen <jacobsen@f600a6c0-e53c-4c81-ba93-d4b609af4611>
2013-01-19 06:35:58 +0000

    debug

--- trunk/eventtools/ops_assign_set.php
+++ trunk/eventtools/ops_assign_set.php
@@ -658,7 +658,7 @@ if ( $args["best"] ) {
                     //echo '['.$ses_next.'/'.$layout_next.']';
                     if (($layout_next == $layout ) && ($ses_next != $session)) {
                         // alternate session, check space
-                        //echo '[ checking '.$ses_next.' with '.$empty_slots_by_session[$ses_next].']';
+                        echo '[ checking '.$ses_next.' with '.$empty_slots_by_session[$ses_next].']';
                         if ($empty_slots_by_session[$ses_next] == '' || $empty_slots_by_session[$ses_next] == NONE || $empty_slots_by_session[$ses_next] > 0) {
                             // found, move request over and repeat
                             echo 'Move '.$email.' request from '.$reqname_by_rqstr[$email][$pri].' '.$strtdate_by_rqstr[$email][$pri];
