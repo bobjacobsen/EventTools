4fd5cd3c897e3138a2a934b25500b1401af70c93
jacobsen <jacobsen@f600a6c0-e53c-4c81-ba93-d4b609af4611>
2013-01-20 22:51:05 +0000

    clean up index to make it a more usable front page

--- trunk/eventtools/index.php
+++ trunk/eventtools/index.php
@@ -38,7 +38,7 @@ if ($event_tools_option_layout_tours)   echo '<td><a href="edit_layout_tour_all.
 if ($event_tools_option_other_events)   echo '<td><a href="edit_misc.php">Enter/Change</a></td>';
 if ($event_tools_option_layouts)        echo '<td><a href="edit_layouts_all.php">Enter/Change</a></td>';
 if ($event_tools_option_clinics)        echo '<td><a href="edit_clinics.php">Enter/Change</a></td>';
-if ($event_tools_option_op_sessions)    echo '<td><a href="edit_ops_add_layout.php">Enter/Change</a></td>';
+if ($event_tools_option_op_sessions)    echo '<td><a href="edit_ops_add_layout.php">Enter/Change Sessions</a></td>';
 
 echo '
 </tr><tr>
@@ -70,7 +70,7 @@ if ($event_tools_option_layout_tours)   echo '<td><a href="index_layout_tours.ph
 if ($event_tools_option_other_events)   echo '<td><a href="index_misc_events.php">Index</a></td>';
 if ($event_tools_option_layouts)        echo '<td><a href="index_layouts.php">Index</a></td></td>';
 if ($event_tools_option_clinics)        echo '<td><a href="index_clinics.php">Index</a></td>';
-if ($event_tools_option_op_sessions)    echo '<td><a href="index_ops.php">Index</a></td>';
+if ($event_tools_option_op_sessions)    echo '<td><a href="index_ops.php">Session Index</a></td>';
 
 echo '</tr><tr>
     <th>Format Example</th>';
@@ -96,7 +96,8 @@ if ($event_tools_option_layouts)        echo '<td>
     </td>';
 if ($event_tools_option_clinics)        echo '<td>
     </td>';
-if ($event_tools_option_op_sessions)    echo '<td><a href="ops_req.php">User edit session request</a></td>';
+if ($event_tools_option_op_sessions)    echo '<td>
+    </td>';
 
 echo '</tr><tr>
     <th>Other Displays</th>';
@@ -119,14 +120,13 @@ if ($event_tools_option_clinics)        echo '<td><a href="format_clinics_by_loc
         <a href="calendar/show_clinics_cal.php">Interactive Clinics Calendar<a/>
     </td>';
 if ($event_tools_option_op_sessions)    echo '<td><a href="ops_req_summary.php">Requests Summary<a/><p>
-    <a href="ops_req_single.php">Single Attendee Record<a/><p>
     <a href="format_all_ops.php">Operating Layout Table</a><p>
     <a href="format_ops_by_day.php?where=status_code%3E40">Operating Sessions vs Day</a><p> 
-    <a href="ops_assign_group.php">Start Assignments with Grouping</a>  <p>  
-    <a href="ops_list_group.php">Email list for cycle</a>    <p>    
+    <hr>
+    <a href="ops_assign_group.php">Start Assignments with Grouping</a>  <p> 
+    The following display a given assignment cycle.<p> 
     <a href="ops_print_by_layout.php">Session Rosters</a>    <p>
     <a href="ops_print_summary.php">Printable Session Rosters</a>    <p>
-    <a href="ops_print_by_attendee.php">Session by Attendee</a>    <p>
     </td>';
     
 echo '</td>
@@ -151,16 +151,23 @@ if (!$event_tools_option_general_tours ||
 
 
 if ($event_tools_option_zen_cart_used) {
-    echo '<p>Attendee list is being maintained in Zen Cart.</p>';
+    echo '<p>Contact and attendee lists are being maintained in Zen Cart.</p>';
 } else {
     echo '<p>';
     echo '<table border="1">';
-    echo '<tr><th>Attendees</th><td>';
+    echo '<tr><th>Contacts<br/></th><td>';
+    echo '<a href="index_attendees.php">Contacts Summary Table</a><p>';
     echo '<a href="edit_zen_repl_customer.php">View/Edit contact info</a><br/>';
     echo '<a href="edit_zen_repl_address.php">View/Edit addresses</a><p>';
-    echo '<a href="index_attendees.php">Table of info</a><p>';
-    if ($event_tools_ops_session_by_category) echo '<a href="edit_user_req_group.php">View/Edit attendee category numbers</a><p>';
-    echo '</td></tr></table>';
+    echo '</td><td>Contacts are people who have pre-registered<br/>or are otherwise known to the committee.</td></tr>';
+    echo '<tr><th>Attendees</th><td>';
+    echo '<a href="ops_reg_attendee_summary.php">Attendee Summary Table<a/><p>';
+    if ($event_tools_ops_session_by_category) echo '<a href="edit_user_req_group.php">View/Edit Attendee Category Numbers</a><p>';
+    echo '<a href="ops_req_single.php">Single Attendee Summary (enter email)<a/><p>';
+    echo '<a href="ops_req.php">Form for users to change their own request (enter email)</a><p>';
+    echo '<a href="ops_print_by_attendee.php">Assigned Sessions by Attendee (enter cycle)</a><p>';
+    echo '<a href="ops_list_group.php">Email list for attendees (enter cycle)</a>    <p>';
+    echo '</td><td>Attendees have registered and/or<br/>requested operating session<br/>assignments<p>They also appear in the contacts pages,<br/>where you can edit their contact info.</td></tr></table>';
 }
 
 ?>
@@ -179,108 +186,140 @@ an existing one if you want to leave existing events unchanged.
 </td>
 </tr></table>
 
-<p>
-EventTools can optionally require that only certain
-values be entered in tables, for example for scale or gauge.
-If you're using that feature, enter the acceptable values using
-the following links.
-<table border="1">
-<tr><th>Character</th><th>Construction</th><th>Operation</th></tr>
 <?php
 
-echo '<tr><td>';
-if ($event_tools_constrain_scale)  echo '<a href="edit_constrain_scale.php" >Scales</a>';
-echo '</td><td>';
-if ($event_tools_constrain_scenery)  echo '<a href="edit_constrain_scenery.php" >Scenery Completion</a>';
-echo '</td><td>';
-if ($event_tools_constrain_ops_scheme)  echo '<a href="edit_constrain_ops_scheme.php" >Ops Scheme</a>';
-echo '</td></tr>';
+if (
+        $event_tools_constrain_scale ||
+        $event_tools_constrain_gauge ||
+        $event_tools_constrain_era   ||
+        $event_tools_constrain_class ||
+        $event_tools_constrain_theme ||
+        $event_tools_constrain_locale ||
 
-echo '<tr><td>';
-if ($event_tools_constrain_gauge)  echo '<a href="edit_constrain_gauge.php" >Gauges</a>';
-echo '</td><td>';
-if ($event_tools_constrain_plan_type)  echo '<a href="edit_constrain_plan_type.php" >Plan Types</a>';
-echo '</td><td>';
-if ($event_tools_constrain_rigor)  echo '<a href="edit_constrain_rigor.php" >Rigor</a>';
-echo '</td></tr>';
+        $event_tools_constrain_scenery ||
+        $event_tools_constrain_plan_type ||
+        $event_tools_constrain_ops_scheme ||
+        $event_tools_constrain_control ||
+        $event_tools_constrain_fidelity ||
+        $event_tools_constrain_rigor ||
+        $event_tools_constrain_documentation ||
+        $event_tools_constrain_session_pace ||
+        $event_tools_constrain_car_forwarding ||
+        $event_tools_constrain_tone ||
+        $event_tools_constrain_dispatched_by1 ||
+        $event_tools_constrain_dispatched_by2 ||
+        $event_tools_constrain_communications ) {
+    echo "
+        <p>
+        EventTools can optionally require that only certain
+        values be entered in tables, for example for scale or gauge.
+        If you're using that feature, enter the acceptable values using
+        the following links.
+        <table border=\"1\">
+        <tr><th>Character</th><th>Construction</th><th>Operation</th></tr>
+    ";
 
-echo '<tr><td>';
-if ($event_tools_constrain_era)    echo '<a href="edit_constrain_era.php"   >Eras</a>';
-echo '</td><td>';
-if ($event_tools_constrain_control)  echo '<a href="edit_constrain_control.php" >Control Systems</a>';
-echo '</td><td>';
-if ($event_tools_constrain_documentation)  echo '<a href="edit_constrain_documentation.php" >Documentation</a>';
-echo '</td></tr>';
+    echo '<tr><td>';
+    if ($event_tools_constrain_scale)  echo '<a href="edit_constrain_scale.php" >Scales</a>';
+    echo '</td><td>';
+    if ($event_tools_constrain_scenery)  echo '<a href="edit_constrain_scenery.php" >Scenery Completion</a>';
+    echo '</td><td>';
+    if ($event_tools_constrain_ops_scheme)  echo '<a href="edit_constrain_ops_scheme.php" >Ops Scheme</a>';
+    echo '</td></tr>';
 
-echo '<tr><td>';
-if ($event_tools_constrain_class)  echo '<a href="edit_constrain_class.php" >Railroad Classes</a>';
-echo '</td><td>';
-if ($event_tools_constrain_fidelity)  echo '<a href="edit_constrain_fidelity.php" >Fidelity</a>';
-echo '</td><td>';
-if ($event_tools_constrain_session_pace)  echo '<a href="edit_constrain_session_pace.php" >Session Pace</a>';
-echo '</td></tr>';
+    echo '<tr><td>';
+    if ($event_tools_constrain_gauge)  echo '<a href="edit_constrain_gauge.php" >Gauges</a>';
+    echo '</td><td>';
+    if ($event_tools_constrain_plan_type)  echo '<a href="edit_constrain_plan_type.php" >Plan Types</a>';
+    echo '</td><td>';
+    if ($event_tools_constrain_rigor)  echo '<a href="edit_constrain_rigor.php" >Rigor</a>';
+    echo '</td></tr>';
 
-echo '<tr><td>';
-if ($event_tools_constrain_theme)  echo '<a href="edit_constrain_theme.php" >Themes</a>';
-echo '</td><td>';
-echo '</td><td>';
-if ($event_tools_constrain_car_forwarding)  echo '<a href="edit_constrain_car_forwarding.php" >Car Forwarding</a>';
-echo '</td></tr>';
+    echo '<tr><td>';
+    if ($event_tools_constrain_era)    echo '<a href="edit_constrain_era.php"   >Eras</a>';
+    echo '</td><td>';
+    if ($event_tools_constrain_control)  echo '<a href="edit_constrain_control.php" >Control Systems</a>';
+    echo '</td><td>';
+    if ($event_tools_constrain_documentation)  echo '<a href="edit_constrain_documentation.php" >Documentation</a>';
+    echo '</td></tr>';
 
-echo '<tr><td>';
-if ($event_tools_constrain_locale) echo '<a href="edit_constrain_locale.php">Locales</a>';
-echo '</td><td>';
-echo '</td><td>';
-if ($event_tools_constrain_tone)  echo '<a href="edit_constrain_tone.php" >Tone</a>';
-echo '</td></tr>';
+    echo '<tr><td>';
+    if ($event_tools_constrain_class)  echo '<a href="edit_constrain_class.php" >Railroad Classes</a>';
+    echo '</td><td>';
+    if ($event_tools_constrain_fidelity)  echo '<a href="edit_constrain_fidelity.php" >Fidelity</a>';
+    echo '</td><td>';
+    if ($event_tools_constrain_session_pace)  echo '<a href="edit_constrain_session_pace.php" >Session Pace</a>';
+    echo '</td></tr>';
 
-echo '<tr><td>';
-echo '</td><td>';
-echo '</td><td>';
-if ($event_tools_constrain_dispatched_by1)  echo '<a href="edit_constrain_dispatched_by1.php" >Dispatching (Primary)</a>';
-echo '</td></tr>';
+    echo '<tr><td>';
+    if ($event_tools_constrain_theme)  echo '<a href="edit_constrain_theme.php" >Themes</a>';
+    echo '</td><td>';
+    echo '</td><td>';
+    if ($event_tools_constrain_car_forwarding)  echo '<a href="edit_constrain_car_forwarding.php" >Car Forwarding</a>';
+    echo '</td></tr>';
 
-echo '<tr><td>';
-echo '</td><td>';
-echo '</td><td>';
-if ($event_tools_constrain_dispatched_by2)  echo '<a href="edit_constrain_dispatched_by2.php" >Dispatching (secondary)</a>';
-echo '</td></tr>';
+    echo '<tr><td>';
+    if ($event_tools_constrain_locale) echo '<a href="edit_constrain_locale.php">Locales</a>';
+    echo '</td><td>';
+    echo '</td><td>';
+    if ($event_tools_constrain_tone)  echo '<a href="edit_constrain_tone.php" >Tone</a>';
+    echo '</td></tr>';
 
-echo '<tr><td>';
-echo '</td><td>';
-echo '</td><td>';
-if ($event_tools_constrain_communications)  echo '<a href="edit_constrain_communications.php" >Communications</a>';
-echo '</td></tr>';
+    echo '<tr><td>';
+    echo '</td><td>';
+    echo '</td><td>';
+    if ($event_tools_constrain_dispatched_by1)  echo '<a href="edit_constrain_dispatched_by1.php" >Dispatching (Primary)</a>';
+    echo '</td></tr>';
+
+    echo '<tr><td>';
+    echo '</td><td>';
+    echo '</td><td>';
+    if ($event_tools_constrain_dispatched_by2)  echo '<a href="edit_constrain_dispatched_by2.php" >Dispatching (secondary)</a>';
+    echo '</td></tr>';
+
+    echo '<tr><td>';
+    echo '</td><td>';
+    echo '</td><td>';
+    if ($event_tools_constrain_communications)  echo '<a href="edit_constrain_communications.php" >Communications</a>';
+    echo '</td></tr></table>';
+}
+
+if ( $event_tools_option_general_tours ||
+       $event_tools_option_layout_tours ||
+       $event_tools_option_other_events ||
+       $event_tools_option_clinics ) {
+    
+    echo '
+        <p>
+        The following links provide formatted information for inclusion in the 
+        printed program.
+        <table border="1">
+        <tr><th>Program Printing</th>
+        <td>
+        <a href="ip_layout_tours.php">Layout Tours<a/><br/>
+        <a href="ip_general_tours.php?type=P">Prototype Tours<a/><br/>
+        <a href="ip_general_tours.php?type=G">General Tours<a/><br/>
+        <a href="ip_clinics.php">Clinics by time<a/><br/>
+        <a href="ip_clinics.php?order=presenter">Clinics by presenter field<a/><br/>
+        <a href="index_clinics.php?tag=NASG">Clinics by tag (fill in name in URL)<a/><br/>
+        </td>
+        </tr></table>
+
+        <p>
+        The following links are to pages that provide information for specific purposes.
+        <table border="1">
+        <tr><th>Special Displays</th>
+        <td>
+        <a href="index_advanced_section.php">Index Advanced Section Tours<a/><br/>
+        <a href="format_advanced_section.php">All Advanced Section Tours<a/><p/>
+        <a href="calendar_form.php">Download Events Calendar<a/><p/>
+        <a href="calendar/show_all_cal.php">Full Interactive Calendar<a/><p/>
+        <a href="calendar_guidebook_form.php">Download for Guidebook install<a/>
+        </td>
+        </tr></table>
+    ';
+}
 ?>
-</table>
-
-<p>
-The following links provide formatted information for inclusion in the 
-printed program.
-<table border="1">
-<tr><th>Program Printing</th>
-<td>
-<a href="ip_layout_tours.php">Layout Tours<a/><br/>
-<a href="ip_general_tours.php?type=P">Prototype Tours<a/><br/>
-<a href="ip_general_tours.php?type=G">General Tours<a/><br/>
-<a href="ip_clinics.php">Clinics by time<a/><br/>
-<a href="ip_clinics.php?order=presenter">Clinics by presenter field<a/><br/>
-<a href="index_clinics.php?tag=NASG">Clinics by tag (fill in name in URL)<a/><br/>
-</td>
-</tr></table>
-
-<p>
-The following links are to pages that provide information for specific purposes.
-<table border="1">
-<tr><th>Special Displays</th>
-<td>
-<a href="index_advanced_section.php">Index Advanced Section Tours<a/><br/>
-<a href="format_advanced_section.php">All Advanced Section Tours<a/><p/>
-<a href="calendar_form.php">Download Events Calendar<a/><p/>
-<a href="calendar/show_all_cal.php">Full Interactive Calendar<a/><p/>
-<a href="calendar_guidebook_form.php">Download for Guidebook install<a/>
-</td>
-</tr></table>
 
 <p>
 For more information on using EventTools, please see the
