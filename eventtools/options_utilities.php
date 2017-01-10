e362ccf293f70927857f30d7cb54b60652b871c1
Bob Jacobsen <jacobsen@mac.com>
2017-01-10 01:23:01 -0800

    new file to old options code

---
+++ eventtools/options_utilities.php
@@ -0,0 +1,66 @@
+<?php
+
+// -------------------------------------------------------------------------
+// Part of EventTools, a package for managing X2011west information
+//
+// By Bob Jacobsen, rgj1927@pacbell.net, Copyright 2010, 2011, 2016
+// -------------------------------------------------------------------------
+
+//
+// Utilities for handling the customer options
+//
+
+// -------------------------------------------------------------------------
+// return a SELECT statement 
+// (not terminated; follow by WHERE, ORDER, etc and ; as needed)
+// that gives access to the user selections as value0 through valueN columns
+// 
+function options_select_statement() {
+    global $event_tools_db_prefix;
+    // get the list of extras
+    $queryExtras="
+        SELECT *
+            FROM ( 
+            ".$event_tools_db_prefix."eventtools_customer_options
+            ) 
+            ORDER BY customer_option_order 
+            ;
+        ";
+    // echo $queryExtras;
+    $resultExtras=mysql_query($queryExtras);
+    $numExtras= mysql_numrows($resultExtras);
+
+    $query="
+        SELECT customers_firstname, customers_lastname, opsreq_person_email, customers_create_date, customers_updated_date, opsreq_priority
+        ";
+    
+    // loop over extras and add column 
+    $i = 0;
+    while ($i < $numExtras) {
+        $query=$query.", table".$i.".customer_option_value_value AS value".$i."  ";
+        $i++;
+    }
+  
+    // add main join
+    $query=$query."
+            FROM ( 
+            ".$event_tools_db_prefix."eventtools_opsession_req LEFT JOIN ".$event_tools_db_prefix."customers
+            ON ".$event_tools_db_prefix."eventtools_opsession_req.opsreq_person_email = ".$event_tools_db_prefix."customers.customers_email_address ";
+
+
+    // loop over extras and add join 
+    $i = 0;
+    while ($i < $numExtras) {
+        $query=$query." LEFT JOIN ".$event_tools_db_prefix."eventtools_customer_option_values AS table".$i." 
+                ON ".$event_tools_db_prefix."customers.customers_id =  table".$i.".customers_id  AND table".$i.".customer_option_id = '".mysql_result($resultExtras,$i,"customer_option_id")."' ";
+        $i++;
+    }
+    
+    $query = $query." ) ";
+
+    //echo $query;
+    return $query;
+}
+
+
+?>
