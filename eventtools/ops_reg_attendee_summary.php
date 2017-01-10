98ae4853f5f82533ea8513b4b432d269e5e356ce
Bob Jacobsen <jacobsen@mac.com>
2017-01-10 01:23:37 -0800

    use utility code

--- eventtools/ops_reg_attendee_summary.php
+++ eventtools/ops_reg_attendee_summary.php
@@ -34,6 +34,7 @@ $update_quantity =  FALSE;         // TRUE to update quantity, FALSE to leave un
 // Code starts here
 
 require_once('utilities.php');
+require_once('options_utilities.php');
 
 global $stats;
 
@@ -91,34 +92,10 @@ if (strlen($args["order"])>5 AND substr($args["order"],0,5) == "value") $order =
 else $order = parse_order();
 
 if ($order == NONE) $order = "customers_lastname";
-$query="
-    SELECT customers_firstname, customers_lastname, opsreq_person_email, customers_create_date, customers_updated_date, opsreq_priority
-    ";
-    
-// loop over extras and add column 
-$i = 0;
-while ($i < $numExtras) {
-    $query=$query.", table".$i.".customer_option_value_value AS value".$i."  ";
-    $i++;
-}
-  
-// add main join
-$query=$query."
-        FROM ( 
-        ".$event_tools_db_prefix."eventtools_opsession_req LEFT JOIN ".$event_tools_db_prefix."customers
-        ON ".$event_tools_db_prefix."eventtools_opsession_req.opsreq_person_email = ".$event_tools_db_prefix."customers.customers_email_address ";
 
-
-// loop over extras and add join 
-$i = 0;
-while ($i < $numExtras) {
-    $query=$query." LEFT JOIN ".$event_tools_db_prefix."eventtools_customer_option_values AS table".$i." 
-            ON ".$event_tools_db_prefix."customers.customers_id =  table".$i.".customers_id  AND table".$i.".customer_option_id = '".mysql_result($resultExtras,$i,"customer_option_id")."' ";
-    $i++;
-}
-       
+$query=options_select_statement();  // create default select statement
+     
 $query=$query."
-        ) 
         ".$where." ORDER BY ".$order." 
         ;
     ";
