33d30cac1cf06660c9011c6733098b35bf70e08c
Bob Jacobsen <jacobsen@mac.com>
2020-01-18 11:55:34 -0800

    documentation

--- open_and_set.php
+++ open_and_set.php
@@ -1,13 +1,31 @@
 <?php
+
+// A generalization of access that also opens the database.
+
+// To be used in two places:
+
+// As a direct replacement of 
+//    global $opts, $event_tools_db_prefix;
+//    mysql_connect($opts['hn'],$opts['un'],$opts['pw']);
+//    @mysql_select_db($opts['db']) or die( "Unable to select database");
+
+// At the top of phpMyEdit files to get the database open
+
+// This is specifically needed when doing INSERT, REPLACE or UPDATE operations,
+// as it sets a mote that handles migration of '' as a decimal value. 
+// Display pages don't need it as such.
+
 include_once('mysql2i.class.php'); // migration step
 
-// Open the database and set a local mode to handle migration of '' as a decimal value
-// stores to $opts[’dbh’] so that open version is used by phpMyEdit
+require_once('access.php');
+
 global $opts, $event_tools_db_prefix;
+// Open the database.
+// Stored handle to $opts[’dbh’] so that open version is used by phpMyEdit
 $opts[’dbh’] = mysql_connect($opts['hn'],$opts['un'],$opts['pw']);
 @mysql_select_db($opts['db']) or die( "Unable to select database");
 
-// set the mode
+// Set a local mode to handle migration of '' as a decimal value
 $query="SET @@LOCAL.sql_mode = ''";
 mysql_query($query);
 
