c55c3c01c9a991f31b6045e7a2fb20ca64d3b2d8
Bob Jacobsen <jacobsen@mac.com>
2023-03-23 12:59:49 -0400

    dates and times partial update

--- access.php
+++ access.php
@@ -3,12 +3,11 @@
 // provide database access constants for phpMyEdit
 // default values - only these should show in SVN
 $opts['hn'] = '127.0.0.1';  // MySQL host name
-$opts['un'] = 'root';       // MySQL user name
-$opts['pw'] = '';           // MySQL password
+$opts['un'] = 'user_name';       // MySQL user name
+$opts['pw'] = 'user_password';           // MySQL password
 
 $opts['db'] = 'database';               // database name to reference
 $event_tools_db_prefix = 'prefix_';     // prefix on all table names
-
 
 // Basic event name for e.g. page headers
 $event_tools_event_name = "(Uncustomised)";
@@ -31,22 +30,21 @@ $event_tools_registrar_email_address = 'rgj1927@pacbell.net';
 $event_tools_registrar_email_prefix = $event_tools_event_name;
 
 // Event timing: These provide defaults for data entry and display structure.
-$event_tools_default_event_start_date   = '2012-07-08 00:00:00';  // default for entry, should be close but not exactly in schedule; c.f. utiltiies.php
-$event_tools_default_event_end_date     = '2012-07-08 01:00:00';  // default for entry, should be close but not exactly in schedule
+$event_tools_default_event_start_date   = '2021-07-06 00:00:00';  // default for entry, should be close but not exactly in schedule; c.f. utiltiies.php
+$event_tools_default_event_end_date     = '2021-07-06 01:00:00';  // default for entry, should be close but not exactly in schedule
 
 $event_tools_dates                      = array( // default days to display
-                                            "2011-07-01","2011-07-02","2011-07-03","2011-07-04","2011-07-05",
-                                            "2011-07-06","2011-07-07","2011-07-08","2011-07-09", "2011-07-10");
+                                            "2021-07-06","2021-07-07","2021-07-08","2021-07-09", "2021-07-10");
 
 $event_tools_clinics_start_times        = array(  // c.f. locale.php
-                                            "08:00","08:30","09:00","09:30","10:00","10:30","11:00","11:30","12:00",
-                                            "13:00","13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00",
-                                            "19:00","19:30","20:00","20:30","21:00","21:30","22:00","22:30");
+                                            "08:00","09:00","10:00","11:00","12:00",
+                                            "13:00","14:00","15:00","16:00","17:00",
+                                            "19:00","20:00","21:00","22:00");
 
 $event_tools_misc_event_start_times     = $event_tools_clinics_start_times; // can also be specified separately
 
 $event_tools_layout_tour_start_times    = array( // c.f. format_layout_tours_by_time.php
-                                            "08:00:00", "09:30:00", "11:00:00", "13:00:00", "14:30:00", 
+                                            "08:00:00", "09:30:00", "11:00:00", "13:00:00", "14:30:00",
                                             "16:00:00", "17:00:00", "19:00:00", "20:30:00", "22:00:00");
 
 $event_tools_layout_tour_dates          = array( // c.f. format_layout_tours_by_time.php; might includes extras before/after main dates
@@ -54,7 +52,7 @@ $event_tools_layout_tour_dates          = array( // c.f. format_layout_tours_by_
                                             "2011-07-04", "2011-07-05", "2011-07-06",
                                             "2011-07-07", "2011-07-08", "2011-07-09");
 
-   
+
 // optional components - TRUE means present (default)
 
 $event_tools_option_general_tours       = FALSE;
@@ -116,7 +114,7 @@ $event_tools_option_zen_cart_used       = FALSE;
 // how to generate a link to optional Zen Cart
 $event_tools_cartlink = "http://127.0.0.1/localcart";
 
-// Include support for doing op-session requests by category? 
+// Include support for doing op-session requests by category?
 $event_tools_ops_session_by_category     = TRUE;
 
 // Do op-session requests By Layout (TRUE) or By Session (FALSE)
